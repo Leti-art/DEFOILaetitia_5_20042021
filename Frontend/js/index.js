ae57a4555c1a8e0725676a5a7f3499239f22b343
Laetitia Defoi <defoi.laetitia@gmail.com>
2021-09-17 14:51:10 +0200

    produit.js début

--- Frontend/js/index.js
+++ Frontend/js/index.js
@@ -1,40 +1,53 @@
-//Fonction principale récupère et affiche les produits sur la page
-(async function() {
-    const oursons = await getOursons()
-})
+let oursons;
+const results = document.getElementById("results");
+let showProduit = document.getElementsByClassName("ourson-produit");
 
+// Api request, fonction async
 
-//Fonction qui permet de récupérer les produits dans l'API
-function getOursons() {
-    //On saisit l'URL de l'API, on utilise fetch
-    return fetch("http://localhost:3000/api/teddies")
-      //Les promesses
-    .then(function(httpBodyResponse) {
-        return httpBodyResponse.json()
-    })
-    .then(function(oursons) {
-        return oursons
-    })
-   
-    
-    //si jamais il y a une erreur, on affiche error à l'utilisateur
-    .catch(function(error){
-        alert(error)
-    })
-}
+const fetchOursons = async () => {
+    oursons = await fetch("http://localhost:3000/api/teddies")
+    .then(res => res.json());
 
-//Fonction permettant d'afficher les produits dans le HTML
-function displayOurson(oursons) {
-    oursons.forEach(function(produit) {
+};
 
-// récupère les informations des produits depuis l'API pour les afficher ensuite
+const showOursons = async () => {
+    await fetchOursons();
 
-     titre.textContent = produit.name;
-     description.textContent = produit.description;
-     prix.textContent = "Prix: " + produit.price + " €";
-     img = produit.imageUrl;
+    results.innerHTML = (
         
-    })
-    
-}
+        oursons
+        .filter(ourson => ourson.name.toLowerCase())
+        .map(ourson => (
+//on map pour afficher les produits, avec du HTML entre les strings inversés
+            `
+            <div class="ourson-item">
+                <img class="ourson-img" src="${ourson.imageUrl}" />
+                <h3 class="ourson-name">${ourson.name}</h3>
+                <div class="ourson-info">
+                    <h2 class="ourson-description">${ourson.description}</h2>
+                    <h3 class="ourson-prix">Prix: ${ourson.price} €</h3>
+                    <select class="ourson-select">
+                    <option value="">Choississez une couleur</option>
+                    ${ourson.colors[0] ? `<option value="">${ourson.colors[0]}</option>` : null}
+                    ${ourson.colors[1] ? `<option value="">${ourson.colors[1]}</option>` : null}
+                    ${ourson.colors[2] ? `<option value="">${ourson.colors[2]}</option>` : null}
+                    ${ourson.colors[3] ? `<option value="">${ourson.colors[3]}</option>` : null}
+                    ${ourson.colors[4] ? `<option value="">${ourson.colors[4]}</option>` : null}
+                    ${ourson.colors[5] ? `<option value="">${ourson.colors[5]}</option>` : null}
+                    </select>
+                    <a href="./produit.html"${ourson._id}" class="ourson-produit">Voir le produit</a>
+                </div>
+                                                                                                          
+            </div>
+            
+            
+            `
 
+        )).join('')
+        //finir par join pour éviter qu'ils se séparent par des virgules
+    );
+
+    
+};
+
+showOursons();
