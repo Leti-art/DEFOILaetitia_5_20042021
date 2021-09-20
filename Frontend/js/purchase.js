cf1a5ad8863400947f2772f19affbe711ebe89a7
Laetitia Defoi <defoi.laetitia@gmail.com>
2021-09-20 21:50:07 +0200

    vérification des données du formulaire

--- Frontend/js/purchase.js
+++ Frontend/js/purchase.js
@@ -41,3 +41,57 @@ clearPurchase.onclick = () => {
     document.location.reload();
 };
 
+//partie formulaire et vérification des données entrées
+
+// vérification du format adresse mail
+
+const checkInputEmail = (input) => {                               
+    if (input.length <=1 || !input.match(/^[a-zA-Z0-9.!#$%&’*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/)) {           
+        alert("Veuillez vérifier votre adresse E-mail !");
+        return false;
+    }
+    return true;
+};
+
+// vérification nom + prénom + ville: verifie <1 lettre et pas de chiffre
+const checkInputLastName = (input) => {                                
+    if (input.length <=1 || input.match(/[0-9]/g)){               
+        alert("Nom mal saisi."); 
+        return false;       
+    }
+    return true;   
+};
+
+const checkInputFirstName = (input) => {                                
+    if (input.length <=1 || input.match(/[0-9]/g)){               
+        alert("Prénom mal saisi."); 
+        return false;       
+    }
+    return true;   
+};
+
+const checkInputCity = (input) => {                                
+    if (input.length <=1 || input.match(/[0-9]/g)){               
+        alert("Veuillez saisir un nom de ville."); 
+        return false;       
+    }
+    return true;   
+};
+
+// vérification de l'adresse: verifie <1 lettre
+const checkInputAddress = (input) => {                             
+    if (input.length <=2) {
+        alert("Veuillez vérifier votre adresse.");
+        return false;
+    }
+    return true;
+};
+
+// véirification du code postale et de la ville 
+const checkInputCodePostal = (input) => {                                
+    if (!input.match(/^([0-9]{5}) (.*)$/g)){               
+        alert("Veuillez corriger votre Code postal.");
+        return false;
+    }
+    return true;
+};
